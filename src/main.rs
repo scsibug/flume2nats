ab552fc7d044fcfdd6b319596fbc4ea297734de3
Greg Heartsfield <scsibug@imap.cc>
2020-12-21 11:00:02 -0600

    Extract config reading to function.

--- src/main.rs
+++ src/main.rs
@@ -14,14 +14,21 @@ struct Credential {
     client_secret: String
 }
 
+fn read_config() -> Result<AppConfig, serde_yaml::Error> {
+    let cfgfile = "config.yml";
+    let cfgstr = fs::read_to_string(cfgfile).expect("failed to read config");
+    let cfgr : Result<AppConfig, serde_yaml::Error> = serde_yaml::from_str(&cfgstr);
+    return cfgr;
+}
+
 #[allow(unused_variables)]
 fn main() {
     println!("Flume2Nats starting up...");
     println!("Reading config file");
-    let cfgfile = "config.yml";
-    let cfgstr = fs::read_to_string(cfgfile).expect("failed to read config");
-    let cfgr : Result<AppConfig, serde_yaml::Error> = serde_yaml::from_str(&cfgstr);
+    let cfgr = read_config(); 
     if let Ok(cfg) = cfgr {
         println!("{:?}",cfg);
+    } else {
+        println!("Failed to read app settings");
     }
 }
